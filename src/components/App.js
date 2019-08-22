426cf58901bbac131981b667f7bd09d079464228
CoffeeCodeCreate <b.r.tech95@gmail.com>
2019-08-22 17:41:49 -0400

    Added methods to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends Component{
     super(props);
 
     this.state = {
-      todos: todo_list;
-    }
+      todos: todo_list
+    };
   }
   render(){
     return(
@@ -19,6 +19,34 @@ class App extends Component{
         <h1> Hola Mundo! </h1>
       </div>
     );
+  }
+
+
+  createTask(task)
+  {
+    task = task.trim() //remove white space
+    if(!task){return;}
+    todo_list.add({ task, isCompleted: false });
+    //Update State
+    this.setState({todos:this.state.todos});
+  }
+
+  toggleCompletion(taskID)
+  {
+    todo_list.toggle(taskID);
+    this.setState({todos: this.state.todos});
+  }
+
+  editTask(taskID, task)
+  {
+    todo_list.update(taskID, task);
+    this.setState({todos: this.state.todos});
+  }
+
+  deleteTask(taskID)
+  {
+    todo_list.remove(taskID);
+    this.setState({todo})
   }
 }
 
